70669f4a478d15458091724c0c9202e821e0856a
Steven Chiou <schiou@utdallas.edu>
2018-07-05 17:25:03 -0500

    condensed simulation codes

--- R/simu.R
+++ R/simu.R
@@ -87,7 +87,7 @@ simu <- function(n, cen, scenario, summary = FALSE) {
 
 #' Function to generate the true hazard used in the simulation.
 #'
-#' This function is used to generate the true cumulative hazard function used in the simulation.
+#' This function is used to generate the true cumulative hazard function for ONE ID used in the simulation.
 #'
 #' @param dat is a data.frame prepared by \code{simu}.
 #'
@@ -98,17 +98,14 @@ simu <- function(n, cen, scenario, summary = FALSE) {
 trueHaz <- function(dat) {
     if (attr(dat, "prepBy") != "rocSimu") stop("Inputed data must be prepared by \"simu\".")
     scenario <- attr(dat, "scenario")
-    if (!any(names(dat) == "id")) {
-        dat <- dat %>% arrange(Y)
-        dat$id <- unlist(lapply(split(dat$Y, dat$Y), function(x) 1:length(x)))
-    }
-    ## if (substr(scenario, 1, 1) == "1") dat <- do.call(rbind, lapply(split(dat, dat$id), function(x) x[which.max(x$Y),]))
-    eval(parse(text = paste("trueHaz", scenario, "(dat)", sep = "")))    
+    dat <- dat %>% arrange(Y)
+    cumHaz <- eval(parse(text = paste("trueHaz", scenario, "(dat)", sep = "")))
+    approxfun(x = dat$Y, y = cumHaz, method = "constant", yleft = 0, yright = max(cumHaz))
 }
 
 #' Function to generate the true survival used in the simulation.
 #'
-#' This function is used to generate the true survival function used in the simulation.
+#' This function is used to generate the true survival function for ONE ID used in the simulation.
 #'
 #' @rdname simu
 #' @export
@@ -116,12 +113,9 @@ trueHaz <- function(dat) {
 trueSurv <- function(dat) {
     if (attr(dat, "prepBy") != "rocSimu") stop("Inputed data must be prepared by \"simu\".")
     scenario <- attr(dat, "scenario")
-    if (!any(names(dat) == "id")) {
-        dat <- dat %>% arrange(Y)
-        dat$id <- unlist(lapply(split(dat$Y, dat$Y), function(x) 1:length(x)))
-    }
-    ## if (substr(scenario, 1, 1) == "1") dat <- do.call(rbind, lapply(split(dat, dat$id), function(x) x[which.max(x$Y),]))
-    eval(parse(text = paste("trueSurv", scenario, "(dat)", sep = "")))    
+    dat <- dat %>% arrange(Y)
+    Surv <- eval(parse(text = paste("trueSurv", scenario, "(dat)", sep = "")))
+    approxfun(x = dat$Y, y = Surv, method = "constant", yleft = 1, yright = min(Surv))
 }
 
 #' Function to generate testing sets given training data.
@@ -246,6 +240,7 @@ sim2.1 <- function(n, cen = 0) {
 sim2.2 <- function(n, cen = 0) {
     e <- rbinom(n, 1, .5)
     u <- matrix(rexp(3 * n, 5), n)
+    u <- t(apply(u, 1, sort))
     u1 <- u[,1]
     u2 <- u[,2]
     u3 <- u[,3]
@@ -355,6 +350,7 @@ sim3.1 <- function(n, cen = 0) {
 sim3.2 <- function(n, cen = 0) {
     e <- rbinom(n, 1, .5)
     u <- matrix(rexp(3 * n, 5), n)
+    u <- t(apply(u, 1, sort))
     u1 <- u[,1]
     u2 <- u[,2]
     u3 <- u[,3]
@@ -409,50 +405,228 @@ sim3.3 <- function(n, cen = 0) {
 #' Background functions for true survival and cumulative hazard curves given case and datasets
 #' @keywords internal
 #' @noRd
-trueHaz1.1 <- function(dat) {
-    cumHaz <- with(dat, Y^2 * exp(2 * z1 + 2 * z2))
-    approxfun(x = dat$Y, y = cumHaz, method = "constant", yleft = 0, yright = max(cumHaz))
-}
+trueHaz1.1 <- function(dat) with(dat, Y^2 * exp(2 * z1 + 2 * z2))
+trueSurv1.1 <- function(dat) exp(-with(dat, Y^2 * exp(2 * z1 + 2 * z2)))
 
-trueSurv1.1 <- function(dat) {
-    Surv <- exp(-with(dat, Y^2 * exp(2 * z1 + 2 * z2)))
-    approxfun(x = dat$Y, y = Surv, method = "constant", yleft = 1, yright = min(Surv))
-}
+trueHaz1.2 <- function(dat) with(dat, Y^2 * exp(2 * sin(2 * pi * dat$z1) + 2 * abs(dat$z2 - .5)))
+trueSurv1.2 <- function(dat) with(dat, exp(-Y^2 * exp(2 * sin(2 * pi * dat$z1) + 2 * abs(dat$z2 - .5))))
 
-trueHaz1.2 <- function(dat) {
-    cumHaz <- with(dat, Y^2 * exp(2 * sin(2 * pi * dat$z1) + 2 * abs(dat$z2 - .5)))
-    approxfun(x = dat$Y, y = cumHaz, method = "constant", yleft = 0, yright = max(cumHaz)) 
-}
+trueHaz1.3 <- function(dat) with(dat, -log(pnorm(Y / exp(-2 + 2 * dat$z1 + 2 * dat$z2), sd = .5)))
+trueSurv1.3 <- function(dat) with(dat, pnorm(Y / exp(-2 + 2 * dat$z1 + 2 * dat$z2), sd = .5))
 
-trueSurv1.2 <- function(dat) {
-    Surv <- with(dat, exp(-Y^2 * exp(2 * sin(2 * pi * dat$z1) + 2 * abs(dat$z2 - .5))))
-    approxfun(x = dat$Y, y = Surv, method = "constant", yleft = 1, yright = min(Surv))
-}
-
-trueHaz1.3 <- function(dat) {
-    cumHaz <- with(dat, -log(pnorm(Y / exp(-2 + 2 * dat$z1 + 2 * dat$z2), sd = .5)))
-    approxfun(x = dat$Y, y = cumHaz, method = "constant", yleft = 0, yright = max(cumHaz))
-}
-
-trueSurv1.3 <- function(dat) {
-    Surv <- with(dat, pnorm(Y / exp(-2 + 2 * dat$z1 + 2 * dat$z2), sd = .5))
-    approxfun(x = dat$Y, y = Surv, method = "constant", yleft = 1, yright = min(Surv))
-}
-
-trueHaz1.4 <- function(dat) {
-    cumHaz <- with(dat, -log(pgamma(exp(z2 * Y / z1) / 4 / z2^2, 1 / 4 / z2^2, 1)))
-    approxfun(x = dat$Y, y = cumHaz, method = "constant", yleft = 0, yright = max(cumHaz))
-}
-
-trueSurv1.4 <- function(dat) {
-    Surv <- with(dat, pgamma(exp(z2 * Y / z1) / 4 / z2^2, 1 / 4 / z2^2, 1))
-    approxfun(x = dat$Y, y = Surv, method = "constant", yleft = 1, yright = min(Surv))
-}
+trueHaz1.4 <- function(dat) with(dat, -log(pgamma(exp(z2 * Y / z1) / 4 / z2^2, 1 / 4 / z2^2, 1)))
+trueSurv1.4 <- function(dat) with(dat, pgamma(exp(z2 * Y / z1) / 4 / z2^2, 1 / 4 / z2^2, 1))
 
 trueHaz1.5 <- function(dat) trueHaz1.1(dat)
 trueSurv1.5 <- function(dat) trueSurv1.1(dat)
 
+trueSurv2.1 <- function(dat) {
+    e <- dat$e[1]
+    u <- dat$u[1]
+    z2 <- dat$z2[1]
+    Y <- dat$Y
+    oneSurv <- function(Y) {
+        if (e == 1) {
+            if (Y < u) return(exp(-exp(z2) * Y^2))
+            else return(exp(-(exp(z2) * u^2 + exp(z2 + 1) * (Y^2 - u^2))))
+        }
+        if (e == 0) {
+            if (Y < u) return(exp(-exp(z2 + 1) * Y^2))
+            else return(exp(-(exp(z2 + 1) * u^2 + exp(z2) * (Y^2 - u^2))))
+        }    
+    }
+    return(sapply(Y, oneSurv))
+}
+
+trueHaz2.1 <- function(dat) {
+    e <- dat$e[1]
+    u <- dat$u[1]
+    Y <- dat$Y
+    z2 <- dat$z2[1]
+    oneHaz <- function(Y) {
+        if (e == 1) {
+            if (Y < u) return(exp(z2) * Y^2)
+            else return(exp(z2) * u^2 + exp(z2 + 1) * (Y^2 - u^2))
+        }
+        if (e == 0) {
+            if (Y < u) return(exp(z2 + 1) * Y^2)
+            else return(exp(z2 + 1) * u^2 + exp(z2) * (Y^2 - u^2))
+        }
+    }
+    return(sapply(Y, oneHaz))
+}
+        
+trueHaz2.2 <- function(dat) {
+    e <- dat$e[1]
+    u1 <- dat$u1[1]
+    u2 <- dat$u2[1]
+    u3 <- dat$u3[1]
+    Y <- dat$Y
+    z2 <- dat$z2[1]
+    oneHaz <- function(Y) {
+        if (e == 1) {
+            if (Y < u1) return(exp(z2 + 1) * Y^2)
+            if (Y >= u1 & Y < u2) return(exp(z2) * (exp(1) * u1^2 + Y^2 - u1^2))
+            if (Y >= u2 & Y < u3) return(exp(z2) * (exp(1) * u1^2 + (u2^2 - u1^2) + exp(1) * (Y^2 - u2^2)))
+            if (Y > u3) return(exp(z2) * (exp(1) * u1^2 + u2^2 - u1^2 + exp(1) * (u3^2 - u2^2) + (Y^2 - u3^2)))
+        }
+        if (e == 0) {
+            if (Y < u1) return(exp(z2) * Y^2 )
+            if (Y >= u1 & Y < u2) return(exp(z2) * (u1^2 + exp(1) * (Y^2 - u1^2)))
+            if (Y >= u2 & Y < u3) return(exp(z2) * (u1^2 + exp(1) * (u2^2 - u1^2) + (Y^2 - u2^2)))
+            if (Y > u3) return(exp(z2) * (u1^2 + exp(1) * (u2^2 - u1^2) + (u3^2 - u2^2) + exp(1) * (Y^2 - u3^2)))
+        }
+    }
+    return(sapply(Y, oneHaz))
+}
+
+
+trueSurv2.2 <- function(dat) {
+    e <- dat$e[1]
+    u1 <- dat$u1[1]
+    u2 <- dat$u2[1]
+    u3 <- dat$u3[1]
+    Y <- dat$Y
+    z2 <- dat$z2[1]
+    oneSurv <- function(Y) {
+        if (e == 1) {
+            if (Y < u1) return(exp(-exp(z2 + 1) * Y^2))
+            if (Y >= u1 & Y < u2) return(exp(-(exp(z2) * (exp(1) * u1^2 + Y^2 - u1^2))))
+            if (Y >= u2 & Y < u3) return(exp(-(exp(z2) * (exp(1) * u1^2 + (u2^2 - u1^2) + exp(1) * (Y^2 - u2^2)))))
+            if (Y > u3) return(exp(-(exp(z2) * (exp(1) * u1^2 + u2^2 - u1^2 + exp(1) * (u3^2 - u2^2) + (Y^2 - u3^2)))))
+        }
+        if (e == 0) {
+            if (Y < u1) return(exp(-(exp(z2) * Y^2 )))
+            if (Y >= u1 & Y < u2) return(exp(-(exp(z2) * (u1^2 + exp(1) * (Y^2 - u1^2)))))
+            if (Y >= u2 & Y < u3) return(exp(-(exp(z2) * (u1^2 + exp(1) * (u2^2 - u1^2) + (Y^2 - u2^2)))))
+            if (Y > u3) return(exp(-(exp(z2) * (u1^2 + exp(1) * (u2^2 - u1^2) + (u3^2 - u2^2) + exp(1) * (Y^2 - u3^2)))))
+        }
+    }
+    return(sapply(Y, oneSurv))
+}
+
+    
+trueHaz3.1 <- function(dat) {
+    e <- dat$e[1]
+    u <- dat$u[1]
+    Y <- dat$Y
+    z2 <- dat$z2[1]
+    oneHaz <- function(Y) {
+        if (e == 1) {
+            if (Y < u) return(.1 * exp(z2) * Y)
+            else return(.1 * exp(z2) * u + .1 * exp(z2 + 1) * (Y - u))
+        }
+        if (e == 0) {
+            if (Y < u) return(exp(z2 + 1) * Y^2)
+            else return(.1 * exp(z2 + 1) * u + .1 * exp(z2) * (Y - u))
+        }
+    }
+    return(sapply(Y, oneHaz))
+}
+
+    
+trueSurv3.1 <- function(dat) {
+    e <- dat$e[1]
+    u <- dat$u[1]
+    Y <- dat$Y
+    z2 <- dat$z2[1]
+    oneSurv <- function(Y) {
+        if (e == 1) {
+            if (Y < u) return(exp(-.1 * exp(z2) * Y))
+            else return(exp(-(.1 * exp(z2) * u + .1 * exp(z2 + 1) * (Y - u))))
+        }
+        if (e == 0) {
+            if (Y < u) return(exp(-exp(z2 + 1) * Y^2))
+            else return(exp(-(.1 * exp(z2 + 1) * u + .1 * exp(z2) * (Y - u))))
+        }
+    }
+    return(sapply(Y, oneSurv))
+}
+
+trueHaz3.2 <- function(dat) {
+    e <- dat$e[1]
+    u1 <- dat$u1[1]
+    u2 <- dat$u2[1]
+    u3 <- dat$u3[1]
+    Y <- dat$Y
+    z2 <- dat$z2[1]
+    oneHaz <- function(Y) {
+        if (e == 1) {
+            if (Y < u1) return(.1 * exp(z2 + 1) * Y)
+            if (Y >= u1 & Y < u2) return(.1 * exp(z2) * (exp(1) * u1 + Y - u1))
+            if (Y >= u2 & Y < u3) return(.1 * exp(z2) * (exp(1) * u1 + (u2 - u1) + exp(1) * (Y - u2)))
+            if (Y > u3) return(.1 * exp(z2) * (exp(1) * u1 + (u2 - u1) + exp(1) * (u3 - u2) + (Y - u3)))
+        }
+        if (e == 0) {
+            if (Y < u1) return(.1 * exp(z2) * Y)
+            if (Y >= u1 & Y < u2) return(.1 * exp(z2) * (u1 + exp(1) * (Y - u1)))
+            if (Y >= u2 & Y < u3) return(.1 * exp(z2) * (u1 + exp(1) * (u2 - u1) + (Y - u2)))
+            if (Y > u3) return(.1 * exp(z2) * (u1 + exp(1) * (u2 - u1) + (u3 - u2) + exp(1) * (Y - u3)))
+        }
+    }
+    return(sapply(Y, oneHaz))
+}
+
+trueSurv3.2 <- function(dat) {
+    e <- dat$e[1]
+    u1 <- dat$u1[1]
+    u2 <- dat$u2[1]
+    u3 <- dat$u3[1]
+    Y <- dat$Y
+    z2 <- dat$z2[1]
+    oneSurv <- function(Y) {
+        if (e == 1) {
+            if (Y < u1) return(exp(-.1 * exp(z2 + 1) * Y))
+            if (Y >= u1 & Y < u2) return(exp(-.1 * exp(z2) * (exp(1) * u1 + Y - u1)))
+            if (Y >= u2 & Y < u3) return(exp(-.1 * exp(z2) * (exp(1) * u1 + (u2 - u1) + exp(1) * (Y - u2))))
+            if (Y > u3) return(exp(-.1 * exp(z2) * (exp(1) * u1 + (u2 - u1) + exp(1) * (u3 - u2) + (Y - u3))))
+        }
+        if (e == 0) {
+            if (Y < u1) return(exp(-.1 * exp(z2) * Y))
+            if (Y >= u1 & Y < u2) return(exp(-.1 * exp(z2) * (u1 + exp(1) * (Y - u1))))
+            if (Y >= u2 & Y < u3) return(exp(-.1 * exp(z2) * (u1 + exp(1) * (u2 - u1) + (Y - u2))))
+            if (Y > u3) return(exp(-.1 * exp(z2) * (u1 + exp(1) * (u2 - u1) + (u3 - u2) + exp(1) * (Y - u3))))
+        }
+    }
+    return(sapply(Y, oneSurv))
+}
+
+trueHaz2.3 <- function(dat) {
+    Y <- dat$Y
+    k <- dat$k[1]
+    b <- dat$b[1]
+    z2 <- dat$z2[1]
+    return(2 * exp(b + z2) * (Y * exp(k * Y) / k - (exp(k * Y) - 1) / k^2))
+}
+
+trueSurv2.3 <- function(dat) {
+    Y <- dat$Y
+    k <- dat$k[1]
+    b <- dat$b[1]
+    z2 <- dat$z2[1]
+    return(exp(-(2 * exp(b + z2) * (Y * exp(k * Y) / k - (exp(k * Y) - 1) / k^2))))
+}
+
+trueHaz3.3 <- function(dat) {
+    Y <- dat$Y
+    k <- dat$k[1]
+    b <- dat$b[1]
+    z2 <- dat$z2[1]
+    return(.1 * exp(z2 + b) * (exp(k * Y) - 1) / k)
+}
+
+trueSurv3.3 <- function(dat) {
+    Y <- dat$Y
+    k <- dat$k[1]
+    b <- dat$b[1]
+    z2 <- dat$z2[1]
+    return(exp(-.1 * exp(z2 + b) * (exp(k * Y) - 1) / k))
+}
+
 #' Background functions for generating testing sets given training data
+#'
+#' These functions give ONE draw of test set
+#' 
 #' @keywords internal
 #' @noRd
 simuTest1.1 <- function(dat) {
@@ -472,29 +646,27 @@ simuTest1.5 <- function(dat) {
 
 simuTest2.1 <- function(dat) {
     Y <- sort(unique(dat$Y))
-    n <- length(Y)
-    e <- rbinom(n, 1, .5)
-    u <- rexp(n, 5)
-    data.frame(Y = Y, z1 = e * (Y < u) + (1 - e) * (Y >= u), z2 = runif(1))
+    e <- rbinom(1, 1, .5)
+    u <- rexp(1, 5)
+    data.frame(Y = Y, z1 = e * (Y < u) + (1 - e) * (Y >= u), z2 = runif(1), e = e, u = u)
 }
 
 simuTest2.2 <- function(dat) {
     Y <- sort(unique(dat$Y))
-    e <- rbinom(n, 1, .5)
-    u <- matrix(rexp(3 * n, 5), n)
-    u1 <- u[,1]
-    u2 <- u[,2]
-    u3 <- u[,3]
+    e <- rbinom(1, 1, .5)
+    u <- sort(rexp(3 * 1, 5))
+    u1 <- u[1]
+    u2 <- u[2]
+    u3 <- u[3]
     z1 <- e * ((u1 <= Y) * (Y < u2) + (u3 <= Y)) + (1 - e) * ((Y < u1) + (u2 <= Y) * (Y < u3))
-    data.frame(Y = Y, z1 = z1, z2 = runif(1))
+    data.frame(Y = Y, z1 = z1, z2 = runif(1), e = e, u1 = u1, u2 = u2, u3 = u3)
 }
 
 simuTest2.3 <- function(dat) {
     Y <- sort(unique(dat$Y))
-    n <- length(Y)
-    k <- runif(n, 1, 2)
-    b <- runif(n, 1, 2)
-    data.frame(Y = Y, z1 = k * Y + b, z2 = runif(1))
+    k <- runif(1, 1, 2)
+    b <- runif(1, 1, 2)
+    data.frame(Y = Y, z1 = k * Y + b, z2 = runif(1), k = k, b = b)
 }
 
 simuTest3.1 <- function(dat) simuTest2.1(dat)
