fa01186d044fd1fe8154591b21bccf4778a98b2f
Steven Chiou <schiou@utdallas.edu>
2019-11-12 18:09:07 -0600

    suppress plotting example

--- inst/examples/ex_plot_rocTree.R
+++ inst/examples/ex_plot_rocTree.R
@@ -1,11 +1,10 @@
+\dontrun{
 data(simDat)
 fit <- rocTree(Surv(Time, death) ~ z1 + z2, id = id, data = simDat, ensemble = FALSE)
-
 ## Plot tree
 plot(fit)
-
 ## Plot survival estimates at terminal nodes
 plot(fit, type = "survival")
-
 ## Plot hazard estimates at terminal nodes
 plot(fit, type = "haz")
+}
